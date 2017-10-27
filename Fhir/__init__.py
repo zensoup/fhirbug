a81dd0ff668255791b367cb914275e2038247d0b
Kostalas <kostalas.v@gmail.com>
2017-10-27 22:10:21 +0300

    Fixed Fhir Resource easy imports

---
+++ Fhir/__init__.py
@@ -0,0 +1,19 @@
+'''
+Load all classes defined inside the Resources package
+and export them via the resources namespace.
+'''
+import os
+import importlib
+import inspect
+from types import SimpleNamespace
+
+resource_classes = {}
+for module_file in os.listdir('Fhir/Resources'):
+    if module_file != '__init__.py' and module_file[-3:] == '.py':
+        module_name = module_file[:-3]
+        # TODO I would like this path to be dynamically retrieved but now is not the time
+        module = importlib.import_module('Fhir.Resources.' + module_name)
+        clsmembers = inspect.getmembers(module, inspect.isclass)
+        resource_classes.update({cls_name: cls for cls_name, cls in clsmembers})
+
+resources = SimpleNamespace(**resource_classes)
