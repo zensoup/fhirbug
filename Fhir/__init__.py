9e591318f59539036fc2897fbe0f656cb74d1a2b
Kostalas <kostalas.v@gmail.com>
2017-10-28 15:25:17 +0300

    Change the way dynamic import works in the Fhir module to enhance usage
    You can now do
    >>> from Fhir.resources import Patient

--- Fhir/__init__.py
+++ Fhir/__init__.py
@@ -5,20 +5,20 @@ and export them via the resources namespace.
 import os
 import importlib
 import inspect
-from types import SimpleNamespace
 
-resource_classes = {}
+from Fhir import resources
+
 for module_file in os.listdir('Fhir/Resources'):
     if module_file != '__init__.py' and module_file[-3:] == '.py':
         module_name = module_file[:-3]
         # TODO I would like this path to be dynamically retrieved but now is not the time
         module = importlib.import_module('Fhir.Resources.' + module_name)
         clsmembers = inspect.getmembers(module, inspect.isclass)
-        resource_classes.update({cls_name: cls for cls_name, cls in clsmembers})
+        for cls_name, cls in clsmembers:
+          setattr(resources, cls_name, cls)
 
 # Import extensions
 ext_module = importlib.import_module('Fhir.Resources.extensions')
 clsmembers = inspect.getmembers(ext_module, inspect.isclass)
-resource_classes.update({cls_name: cls for cls_name, cls in clsmembers})
-
-resources = SimpleNamespace(**resource_classes)
+for cls_name, cls in clsmembers:
+  setattr(resources, cls_name, cls)
---
+++ Fhir/resources.py
@@ -0,0 +1,10 @@
+'''
+This file is dynamicaly populated by this module's __init__.py, so it's
+normal to see absolutely nothing in it.
+There isn't really much else to say here, this is starting to get a bit awkward...
+
+I wonder if I can can use the module's dynamic contents in doctests
+
+>>> p = Patient()
+
+'''
