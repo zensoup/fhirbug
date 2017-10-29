bb8690a6b9e25b7dbdd41d0f33faa89b45030288
Kostalas <kostalas.v@gmail.com>
2017-10-29 10:50:01 +0200

    Added a shortcut for get methods on Models

--- db/backends/SQLAlchemy/fhirbasemodel.py
+++ db/backends/SQLAlchemy/fhirbasemodel.py
@@ -2,4 +2,12 @@ from .abstractbasemodel import AbstractBaseModel
 
 class FhirBaseModel(AbstractBaseModel):
   __abstract__ = True
-  pass
+
+  @classmethod
+  def get(cls, id, *args, **kwargs):
+    '''
+    Handle a GET request
+    '''
+    item = cls.query.get(id)
+    res = item.to_fhir(*args, **kwargs)
+    return res.as_json()
