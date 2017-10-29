f3f76c1368806ee501102c69d70610b050b1c9ec
Kostalas <kostalas.v@gmail.com>
2017-10-29 10:51:46 +0200

    Started working on request handling

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
+import importlib
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 
+from http.requestparser import parse_url
 import settings
 
 # Create the db connection
@@ -21,12 +23,25 @@ import models  # Don't do from models import bla, stuff will break
 
 def main():
   # pat = session.query(models.Patient).first()
-  pat = models.Patient.query.first()
+  # pat = models.Patient.query.first()
+  pat = models.Patient.get(940)
 
-  print(pat.to_fhir().as_json())
+  # print(pat.to_fhir().as_json())
+  print(pat)
 
-  o = models.ProcedureRequest.query.first()
-  print(o.to_fhir().as_json())
+  o = models.ProcedureRequest.get(16, contained=['subject'])
+  print(o)
+
+def handle_get_request(url):
+  query = parse_url(url)
+  resource = query['resource']
+  try:
+    Resource = getattr(models, resource)
+  except Exception as e:
+    return {'error': 'resource does not exist'}, 400
+  if query['resourceId']:
+    return Resource.get(query['resourceId']), 200
+
 
 if __name__ == '__main__':
   main()
