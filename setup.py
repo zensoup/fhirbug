6fb075aece4712585dc48d5c76ea93f2e0205c5d
Vangelis Kostalas <kostalas.v@gmail.com>
2017-11-10 10:54:53 +0200

    Hopefully fixed setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='fhirball',
-      version='0.1',
-      packages=['fhirball'],
+      version='0.1.1',
+      packages=find_packages(),
       author="Vagelis Kostalas",
       author_email="lostalas.v@gmail.com",
       description="A Fhir server",
