2d4b036b5f25e0cc39c9812095b42b586c83dca2
Kostalas <kostalas.v@gmail.com>
2017-11-16 21:50:50 +0200

    def __get_query__(cls, query):     return query.filter(cls.name_last.contains('ΒΟΤ'))

--- fhirball/db/backends/SQLAlchemy/base.py
+++ fhirball/db/backends/SQLAlchemy/base.py
@@ -7,14 +7,21 @@ from sqlalchemy.ext.declarative import declarative_base, DeclarativeMeta
 
 
 class AbstractModelMeta(DeclarativeMeta):
-  def __new__(mcs, name, bases, attrs, **kwargs):
-    ret = super().__new__(mcs, name, bases, attrs)
-    print(name, 'query' in bases[0].__dict__)
-    if '__get_query__' in attrs:
-      cc = [cl for cl in bases[0].mro() if 'query' in cl.__dict__]
-      import ipdb; ipdb.set_trace()
-      ret.query = ret.__get_query__(bases[0].query)
-    return ret
+  # def __new__(mcs, name, bases, attrs, **kwargs):
+  #   ret = super().__new__(mcs, name, bases, attrs)
+  #   print(name, 'query' in bases[0].__dict__)
+  #   if '__get_query__' in attrs:
+  #     cc = [cl for cl in bases[0].mro() if 'query' in cl.__dict__]
+  #     import ipdb; ipdb.set_trace()
+  #     ret.query = ret.__get_query__(bases[0].query)
+  #   return ret
+  def __getattribute__(self, item):
+    if item == 'query':
+      if hasattr(self, '__get_query__'):
+        query = super(AbstractModelMeta, self).__getattribute__('query')
+        return self.__get_query__(self, query)
+    return super(AbstractModelMeta, self).__getattribute__(item)
+
 
 
 Base = declarative_base(metaclass=AbstractModelMeta)
