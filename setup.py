0dddfb4b38e3b54cf1899cf7fbdba2b7ef886fbf
Vangelis Kostalas <kostalas.v@gmail.com>
2018-12-21 19:42:36 +0200

    Tiny fixes setup.py and Readme

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='fhirbug',
       version='0.1.1',
       packages=find_packages(),
-      author="Vagelis Kostalas",
+      author="Vangelis Kostalas",
       author_email="kostalas.v@gmail.com",
       description="A Fhir server",
       python_requires=">=3.6",
