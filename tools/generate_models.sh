b56afd5aa1ae79df984ca62f8eaa2f48d020f12d
Vangelis Kostalas <kostalas.v@gmail.com>
2019-01-18 10:20:18 +0200

    Added coverage reports to travis

--- tools/generate_models.sh
+++ tools/generate_models.sh
@@ -8,7 +8,7 @@ TOOLS_DIR=$(dirname "$0")
 cd $TOOLS_DIR
 source fhir_parser/.venv/bin/activate
 
-cp ../Fhir/base/settings.py fhir_parser/settings.py
+cp ../fhirbug/Fhir/base/settings.py fhir_parser/settings.py
 cd fhir_parser
 ./generate.py $1
 cd ..
